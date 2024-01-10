8bb1b899b712b6f6f3e9be7b46536f1821799f46
Savage <snayperlvov1@gmail.com>
2024-01-11 00:42:13 +0300

    add README.md

--- cmd/meal-planner/main_test.go
+++ cmd/meal-planner/main_test.go
@@ -1,49 +1,125 @@
-package main
+package main_test
 
-import (
-	_ "github.com/stretchr/testify/assert"
-	"net/http"
-	"testing"
-)
+//func TestSignInHandler_Success(t *testing.T) {
+//	payload := app.UserRequest{
+//		Login:    "testuser",
+//		Password: "testpassword",
+//	}
+//	payloadBytes, _ := json.Marshal(payload)
+//	req, err := http.NewRequest("POST", "/sign_in", bytes.NewReader(payloadBytes))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	rr := httptest.NewRecorder()
+//	handler := http.HandlerFunc(app.SignIn)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusSeeOther, rr.Code)
+//
+//}
+//
+//func TestSignInHandler_Failure(t *testing.T) {
+//	payload := app.UserRequest{
+//		Login:    "nonexistentuser",
+//		Password: "wrongpassword",
+//	}
+//	payloadBytes, _ := json.Marshal(payload)
+//	req, err := http.NewRequest("POST", "/sign_in", bytes.NewReader(payloadBytes))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	rr := httptest.NewRecorder()
+//	handler := http.HandlerFunc(app.SignIn)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+//}
+//
+//func TestSignUpHandler_Success(t *testing.T) {
+//	payload := app.UserRequest{
+//		Login:    "newuser",
+//		Password: "newpassword",
+//	}
+//	payloadBytes, _ := json.Marshal(payload)
+//	req, err := http.NewRequest("POST", "/sign_up", bytes.NewReader(payloadBytes))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	rr := httptest.NewRecorder()
+//	handler := http.HandlerFunc(app.SignUp)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusOK, rr.Code)
+//
+//}
+//
+//func TestSignUpHandler_Failure(t *testing.T) {
+//
+//	payload := app.UserRequest{
+//		Login:    "testuser",
+//		Password: "testpassword",
+//	}
+//	payloadBytes, _ := json.Marshal(payload)
+//	req, err := http.NewRequest("POST", "/sign_up", bytes.NewReader(payloadBytes))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	rr := httptest.NewRecorder()
+//	handler := http.HandlerFunc(app.SignUp)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+//
+//}
 
-func TestHandler(t *testing.T) {
-	tests := []struct {
-		name                   string
-		method                 string
-		request                string
-		body                   string
-		expectedStatusCode     int
-		expectedLocationHeader string
-		expectedResponseBody   string
-	}{
-		{
-			name:               "POST request with a valid link",
-			method:             http.MethodPost,
-			request:            "/",
-			body:               "https://example.com",
-			expectedStatusCode: http.StatusCreated,
-		},
-		{
-			name:               "POST request with an empty link",
-			method:             http.MethodPost,
-			request:            "/",
-			body:               "",
-			expectedStatusCode: http.StatusBadRequest,
-		},
-		{
-			name:                 "JSON POST request with a valid link",
-			method:               http.MethodPost,
-			request:              "/api/shorten",
-			body:                 `{"url": "https://example.com"}`,
-			expectedStatusCode:   http.StatusCreated,
-			expectedResponseBody: `{"result": "YourShortURLLogicHere"}`,
-		},
-		{
-			name:                 "JSON POST request with an empty link",
-			method:               http.MethodPost,
-			request:              "/api/shorten",
-			body:                 ``,
-			expectedStatusCode:   http.StatusBadRequest,
-			expectedResponseBody: "",
-		},
-	}
+//func TestMainPageHandler_Authenticated(t *testing.T) {
+//	req, err := http.NewRequest("GET", "/", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	session := &app.SessionMock{
+//		Values: map[interface{}]interface{}{
+//			"authenticated": true,
+//		},
+//	}
+//	rr := httptest.NewRecorder()
+//	req = req.WithContext(app.SessionContext(req.Context(), "session", session))
+//
+//	handler := http.HandlerFunc(app.MainPage)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusOK, rr.Code)
+//
+//}
+
+//func TestMainPageHandler_Unauthenticated(t *testing.T) {
+//	req, err := http.NewRequest("GET", "/", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	session := &app.SessionMock{
+//		Values: map[interface{}]interface{}{
+//			"authenticated": false,
+//		},
+//	}
+//	rr := httptest.NewRecorder()
+//	req = req.WithContext(app.SessionContext(req.Context(), "session", session))
+//
+//	handler := http.HandlerFunc(app.MainPage)
+//
+//	handler.ServeHTTP(rr, req)
+//
+//	assert.Equal(t, http.StatusSeeOther, rr.Code)
+//
+//}
