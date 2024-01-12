abccd1aa7c3eec21dec2f2ac5f26aece22d9d3e4
Savage <snayperlvov1@gmail.com>
2024-01-12 13:19:47 +0300

    refactor

--- internal/app/deleteHandler.go
+++ internal/app/deleteHandler.go
@@ -17,45 +17,57 @@ var removingDislikeFood string
 func DeleteFoodHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session")
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
-	} else {
-		if r.Method == http.MethodDelete {
-			err := json.NewDecoder(r.Body).Decode(&nFD)
-			if err != nil {
-				http.Error(w, "Invalid request body", http.StatusBadRequest)
-				return
-			}
-			er := ConnectionDB.DeleteFood(context.Background(), Request.Login, nFD.Foodname)
-			if !er {
-				http.Error(w, "Invalid input data", http.StatusBadRequest)
-			} else {
-				w.WriteHeader(http.StatusAccepted)
-				w.Write([]byte("URLs deleted"))
-			}
-		}
+		http.Error(w, "You are not auth", http.StatusBadRequest)
+		return
 	}
+
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Use DELETE method for this endpoint", http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&nFD)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	ok := ConnectionDB.DeleteFood(context.Background(), Request.Login, nFD.Foodname)
+	if !ok {
+		http.Error(w, "Invalid input data", http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("URLs deleted"))
+
 }
 
 // Хэндлер удаляет получаемый продукт, который пользователь добавил в исключение
 func DeleteDislikeFoodHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := store.Get(r, "session")
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
-	} else {
-		if r.Method == http.MethodDelete {
-			err := json.NewDecoder(r.Body).Decode(&removingDislikeFood)
-			if err != nil {
-				http.Error(w, "Invalid request body", http.StatusBadRequest)
-				return
-			}
-			er := ConnectionDB.DeleteDislikeFood(context.Background(), Request.Login, removingDislikeFood)
-			if !er {
-				http.Error(w, "Invalid input data", http.StatusBadRequest)
-			} else {
-				w.WriteHeader(http.StatusAccepted)
-				w.Write([]byte("URLs deleted"))
-			}
-		}
-
+		http.Error(w, "You are not auth", http.StatusBadRequest)
+		return
 	}
+
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Use DELETE method for this endpoint", http.StatusBadRequest)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&removingDislikeFood)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	ok := ConnectionDB.DeleteDislikeFood(context.Background(), Request.Login, removingDislikeFood)
+	if !ok {
+		http.Error(w, "Invalid input data", http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusAccepted)
+	w.Write([]byte("URLs deleted"))
+
 }
